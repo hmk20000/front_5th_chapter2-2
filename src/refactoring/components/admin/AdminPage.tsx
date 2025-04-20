4e48f01317f53aed5e9297b6e8db5739053e38b5
codeming <hmk2000015@gmail.com>
2025-04-20 20:14:18 +0900

    refactor: 어드민 디렉토리 분리
    Admin 전용 컴포넌트 추가시 Cart 컴포넌트와 혼동 예상.
    어드민 컴포넌트에 Admin을 프리필스 붙일 경우 공용 컴포넌트로 사용시 이름 변경 문제를 야기할 것으로 예상됨.
    따라서 디렉토리로 구분해두고 추후 공용 컴포넌트일 경우 상위 디렉토리로 이동 예정

--- src/refactoring/components/admin/AdminPage.tsx
+++ src/refactoring/components/admin/AdminPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Coupon, Discount, Product } from '../../types.ts';
+import { Coupon, Discount, Product } from '../../../types.ts';
 import NewProduct from './NewProduct.tsx';
 
 interface Props {

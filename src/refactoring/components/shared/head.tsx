1daa4f6d9e8cf62c94d87111c5b10507c083ca40
codeming <hmk2000015@gmail.com>
2025-04-20 22:45:43 +0900

    feature: H4 추가

--- src/refactoring/components/shared/head.tsx
+++ src/refactoring/components/shared/head.tsx
@@ -5,3 +5,7 @@ export const H2 = ({ children }: { children: React.ReactNode }) => {
 export const H3 = ({ children }: { children: React.ReactNode }) => {
   return <h3 className="text-lg font-semibold mb-2">{children}</h3>;
 };
+
+export const H4 = ({ children }: { children: React.ReactNode }) => {
+  return <h4 className="text-base font-semibold mb-2">{children}</h4>;
+};
